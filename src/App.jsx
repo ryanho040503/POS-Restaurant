5710c4dca5718a4bab7083ade0315d927d6308cf
MACBOOK AIR M1 <macbookairm1@Duy-Hoang-2.local>
2025-08-06 16:33:47 -0400

    Finishing protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,62 @@ import { Home, Auth, Orders, Tables } from "./pages";
 import Header from './components/shared/Header';
 import Menu from './pages/Menu';
 import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import useLoadData from './hooks/useLoadData';
+import FullScreenLoader from './components/shared/FullScreenLoader';
 
 function Layout() {
+
   const location = useLocation();
+  const isLoading = useLoadData();
   const hideHeaderRoutes = ["/auth"];
+  const isAuth = useSelector(state => state.user?.isAuth);
+
+  if(isLoading)return <FullScreenLoader />
 
   return (
     <>
       {!hideHeaderRoutes.includes(location.pathname) && <Header />}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/tables" element={<Tables />} />
-        <Route path="/menu" element={<Menu />} />
+        <Route path="/" element={
+          <ProtectedRoutes>
+            <Home />
+          </ProtectedRoutes>
+        } />
+        <Route path="/auth" element={isAuth ? <Navigate to="/" /> : <Auth />} />
+        <Route path="/orders" element={
+          <ProtectedRoutes>
+            <Orders />
+          </ProtectedRoutes>
+        } />
+        <Route path="/tables" element={
+          <ProtectedRoutes>
+            <Tables />
+          </ProtectedRoutes>
+        } />
+        <Route path="/menu" element={
+          <ProtectedRoutes>
+            <Menu />
+          </ProtectedRoutes>
+        } />
         <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
 
     </>
   )
 }
+
+function ProtectedRoutes({children}){
+
+  const { isAuth } = useSelector(state => state.user);
+  if(!isAuth){
+    return <Navigate to='/auth' />
+  }
+
+  return children;
+}
+
 
 function App() {
   return (
